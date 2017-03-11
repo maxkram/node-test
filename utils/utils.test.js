5287d8589385206f53b6ddc0ad02d6227c4d24d3
maxkram <max.kramer@live.com>
2017-03-11 12:41:29 +0300

    test with assertions

---
+++ utils/utils.test.js
@@ -0,0 +1,42 @@
+const expect = require("expect");
+
+const utils = require('./utils');
+
+it('добавим 2 числа', () => {
+  var res = utils.add(33, 11);
+  expect(res).toBe(44).toBeA("number");
+  // if (res !== 44){
+  //   throw new Error(`Ожидалось 44, но получилось ${res}`);
+  // }
+});
+
+it('должен получиться квадрат числа', () => {
+  var res = utils.square(3);
+  expect(res).toBe(9).toBeA("number");
+
+  // if (res !== 9) {
+  //   throw new Error(`Должно быть 9, но получилось ${res}`);
+  // }
+});
+
+it("нужно ввести имя-фамилие", () => {
+  var user = {location: "Baku", age: 33};
+  var res = utils.setName(user, "Kolyan Burkin");
+  expect(res).toInclude({
+    firstName: "Kolyan",
+    lastName: "Burkin"
+  });
+});
+
+// it("здесь должно быть какое-нибудь значение", () => {
+//   // expect(12).toNotBe(12);
+//   // expect({name: "Kolyan"}).toEqual({name: "Kolyan"});
+//   // expect([2,3,4]).toInclude(2);
+//   expect({
+//     name: "Kolyan",
+//     age: 33,
+//     location: "Baku"
+//   }).toInclude({
+//     age: 33
+//   })
+// });
